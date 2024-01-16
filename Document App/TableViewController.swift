18116ba41f2662b80b79150d04424fdbf9ec9868
STRAUBJACQUIN <titouan.straub@etu.univ-smb.fr>
2024-01-16 15:58:45 +0100

    first step

---
+++ Document App/TableViewController.swift
@@ -0,0 +1,110 @@
+//
+//  TableViewController.swift
+//  Document App
+//
+//  Created by Titouan STRAUB on 1/16/24.
+//
+
+import UIKit
+
+struct DocumentFile {
+    var title: String
+    var size: Int
+    var imageName: String? = nil
+    var url: URL
+    var type: String
+    static let data: [DocumentFile] = [DocumentFile(title: "Document 1", size: 100, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+                                       DocumentFile(title: "Document 2", size: 200, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+                                       DocumentFile(title: "Document 3", size: 300, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+                                       DocumentFile(title: "Document 4", size: 400, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+                                       DocumentFile(title: "Document 5", size: 500, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+                                       DocumentFile(title: "Document 6", size: 600, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+                                       DocumentFile(title: "Document 7", size: 700, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+                                       DocumentFile(title: "Document 8", size: 800, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+                                       DocumentFile(title: "Document 9", size: 900, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain"),
+                                       DocumentFile(title: "Document 10", size: 1000, imageName: nil, url: URL(string: "https://www.apple.com")!, type: "text/plain")]
+}
+
+
+
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Document App/readme.md
@@ -0,0 +1,29 @@
+Les targets:
+Il permet de définir les appareils et la version d'IOS cible de l'app et l'orientation de l'écran. Il permet aussi de donner une version de révision (version de sortie), de donner un catégorie 
+a notre appli pour qu'elle soit correctement répertoriée dans l'AppStore, il définit également les framewoks utilisés, les assets et l'icone de l'app.
+
+Les fichiers:
+Ce sont les fichiers de code de l'app, on y trouve également un apprecu de l'app (Main) le LaunchScreen (écran de chargement de l'app') et les Assets.
+
+Les Assets:
+C'est l'endroit ou es répertorié tous les assets que l'on utilise pour notre app, images, vidéos, icone etc...
+
+Le storyBoard:
+C'est notre espace de travail, on peut y ouvrir plusieurs onglets et accedder a différents menus.
+
+Le simulateur:
+Il nous permet de simuler notre app sur le Device choisit (émulateur) (utile pour les developpeurs)
+
+Raccourci CMD + r:
+Permet de RUN l'App
+
+Raccourci CMD + shift + O:
+Permet d'ouvrir un fichier rapidement
+
+Raccourci indentation:
+Ctrl + i
+
+Raccourci commenter la selection:
+CMD + /
+
+Pour changer d'appareil lors de la simulation, il y a une liste déroulante en haut de la fenetre.
